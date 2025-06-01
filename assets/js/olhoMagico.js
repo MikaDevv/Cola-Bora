add3f288ce0d47a929d5b3b3257293c9f729aaf5
MikaDevv <mikaanastacioteixeira@gmail.com>
2025-06-01 17:11:37 -0300

    tela de cadastro concluida

--- assets/js/olhoMagico.js
+++ assets/js/olhoMagico.js
@@ -13,3 +13,20 @@ togglePassword.addEventListener("click", function () {
     togglePassword.src = "/assets/imgs/eye-off.svg";
   }
 });
+
+const passwordInputRepeat = document.getElementById("passwordRepeat");
+const togglePasswordRepeat = document.getElementById("togglePasswordRepeat");
+let passwordVisibleRepeat = false;
+
+togglePasswordRepeat.addEventListener("click", function () {
+  passwordVisibleRepeat = !passwordVisibleRepeat;
+
+  if (passwordVisibleRepeat) {
+    passwordInputRepeat.type = "text";
+    togglePasswordRepeat.src = "/assets/imgs/eye-open.svg";
+  } else {
+    passwordInputRepeat.type = "password";
+    togglePasswordRepeat.src = "/assets/imgs/eye-off.svg";
+  }
+});
+
